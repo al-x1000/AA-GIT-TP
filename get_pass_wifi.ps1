e78c1e55d576e8924f1774efabf314fb1c240b7b
alxis <huelloua@gmail.com>
2022-09-29 11:54:40 +0200

    3rd Script v1.0
    Add the script.

--- get_pass_wifi.ps1
+++ get_pass_wifi.ps1
@@ -6,3 +6,33 @@
 #              HUELLOU Alexis, TAILLANDER Aliaume             #
 #                                                             #
 ###############################################################
+
+$wifi=@()
+
+#Show blocked network
+$cmd0=netsh wlan show blockednetworks
+#List of SSID
+$cmd1=netsh wlan show profiles
+ForEach($row1 in $cmd1)
+{
+    #SSID Recuperation
+    If($row1 -match 'Profil Tous les utilisateurs[^:]+:.(.+)$')
+    {
+        $ssid=$Matches[1]
+        $cmd2=netsh wlan show profiles $ssid key=clear
+        ForEach($row2 in $cmd2)
+        {
+            #Password Recuperation
+            If($row2 -match 'Contenu de la c[^:]+:.(.+)$')
+            {
+                $key=$Matches[1]
+                #We stock the password in a tab
+                $wifi+=[PSCustomObject]@{ssid=$ssid;key=$key}
+            }
+        }
+    }
+}
+#Export the tab into CSV file
+$wifi|Export-CSV -Path 'c:\wifi.csv' -NoTypeInformation
+#Show Tab
+$wifi|Sort -Property ssid|Out-GridView -Title 'Mot de passe des - SSID enregistré'
